5c3ced756b8cb37623da3cbcc967cf1640211118
Mayur Mahajan <mayur23smahajan@gmail.com>
2021-11-24 18:22:43 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SportsMmaIcon from '@mui/icons-material/SportsMma';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+      <SportsMmaIcon />      
+      Keeper
+      </h1>
+      
+    </header>
+  );
+}
+
+export default Header;
